c2ac4bc58560a55308345cdea35d3373bd969cc1
Sony Maia <sonymaia@MacBook-Pro-de-Sony.local>
2023-03-09 23:49:56 -0300

    alterando o end do static 2

--- setup/settings.py
+++ setup/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'setup/static'),]
+ADMIN_MEDIA_PREFIX = '/static/admin'
 
 
 # Default primary key field type
