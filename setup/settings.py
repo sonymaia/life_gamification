57f97fc17da9df4f92cc16b2c776ef20e506d67c
Sony Maia <sonymaia@MacBook-Pro-de-Sony.local>
2023-03-10 00:49:56 -0300

    Debug int

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = bool(int(os.getenv('DEBUG')))
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://livegamification-production.up.railway.app']
