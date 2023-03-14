4b3c649b9e2947a791642b030074af78f158ae74
Sony Maia <sonymaia@MacBook-Pro-de-Sony.local>
2023-03-13 23:35:53 -0300

    alteracao no DB

--- record/admin.py
+++ record/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
-from record.models import Book, Course, Daily_Objective, Rule, Logbook
+from record.models import User, Daily_Objective, Rule, Logbook, Category, Category_Record, Goal
 
 
-class BooksAndCoursesList(admin.ModelAdmin):
-    list_display = ("id", "name", "conclusion_date")
+class UsersList(admin.ModelAdmin):
+    list_display = ("id", "name", "email")
+    list_display_links = ("id", "name", "email")
+    search_fields = ("name", "email",)
+admin.site.register(User, UsersList)
+    
+class CategoriesList(admin.ModelAdmin):
+    list_display = ("id", "name")
     list_display_links = ("id", "name")
     search_fields = ("name",)
-admin.site.register(Book, BooksAndCoursesList)
-admin.site.register(Course, BooksAndCoursesList)
+admin.site.register(Category, CategoriesList)
 
+class CategoryRecordsList(admin.ModelAdmin):
+    list_display = ("id", "fk_category", "description", "date", "fk_user")
+    list_display_links = ("id", "fk_category", "description")
+    list_filter = ("fk_category","fk_category","fk_user",)
+    list_per_page = 10
+    search_fields = ("description",)
+admin.site.register(Category_Record, CategoryRecordsList)
+
+class GoalsList(admin.ModelAdmin):
+    list_display = ("id", "description", "creation_date", "conclusion_date", "done", "fk_user", "reward")
+    list_display_links = ("id", "description")
+    list_filter = ("fk_user",)
+    list_per_page = 10
+    search_fields = ("description",)
+    list_editable = ("done",)
+admin.site.register(Goal, GoalsList)
 
 class DailyObjsList(admin.ModelAdmin):
     list_display = ("id", "name")
@@ -17,17 +38,23 @@ class DailyObjsList(admin.ModelAdmin):
 admin.site.register(Daily_Objective, DailyObjsList)
 
 class RulesList(admin.ModelAdmin):
-    list_display = ("id", "description", "award")
+    list_display = ("id", "description", "reward")
     list_display_links = ("id", "description")
     search_fields = ("description",)
 admin.site.register(Rule, RulesList)
 
 class logBooksList(admin.ModelAdmin):
-    list_display = ("date", "fk_daily_obj")
+    list_display = ("date", "fk_daily_obj", "fk_user")
     list_display_links = ("date", "fk_daily_obj")
     search_fields = ("date",)
-    list_filter = ("fk_daily_obj",)
+    list_filter = ("fk_daily_obj", "fk_user",)
     list_per_page = 10
-    #list_editable = ("date",)
 admin.site.register(Logbook, logBooksList)
+
+#class BooksAndCoursesList(admin.ModelAdmin):
+#    list_display = ("id", "name", "conclusion_date")
+#    list_display_links = ("id", "name")
+#    search_fields = ("name",)
+#admin.site.register(Book, BooksAndCoursesList)
+#admin.site.register(Course, BooksAndCoursesList)
 
--- record/models.py
+++ record/models.py
@@ -1,33 +1,65 @@
 from django.db import models
 from datetime import datetime
 
+class User(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False)
+    email = models.CharField(max_length=150, null=False, blank=False)
+    password = models.CharField(max_length=15, null=False, blank=False)
+    def __str__(self):
+        return f"User [name = {self.name}]"
+    
 class Daily_Objective(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     def __str__(self):
         return f"Daily_Objective [name = {self.name}]"
 
-class Course(models.Model):
-    name = models.CharField(max_length=300, null=False, blank=False)
-    conclusion_date = models.DateField(null=True, blank=True)
-    def __str__(self):
-        return f"Couse [name = {self.name}]"
-
-
-class Book(models.Model):
-    name = models.CharField(max_length=300, null=False, blank=False)
-    conclusion_date = models.DateField(null=True, blank=True)
-    #conclusion_date = models.DateField(default = datetime.now, null=True, blank=True)
-    def __str__(self):
-        return f"Book [name = {self.name}]"
-
-
 class Rule(models.Model):
     description = models.TextField(null=False, blank=False)
-    award = models.DecimalField(max_digits=10, decimal_places=0)
+    reward = models.DecimalField(max_digits=10, decimal_places=0)
     def __str__(self):
         return f"Rule [name = {self.description}]"
-    
-    
+     
 class Logbook(models.Model):
-    date = models.DateField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True, default = datetime.now)
     fk_daily_obj = models.ForeignKey(Daily_Objective, on_delete=models.CASCADE)
+    fk_user = models.ForeignKey(User, on_delete=models.CASCADE, default = "")
+
+class Category(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False)
+    def __str__(self):
+        return f"Category [name = {self.name}]"
+
+class Category_Record(models.Model):
+    fk_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(null=False, blank=False)
+    date = models.DateField(null=True, blank=True)
+    fk_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Category_Record [description = {self.description}]"
+    
+class Goal(models.Model):
+    description = models.TextField(null=False, blank=False)
+    creation_date = models.DateField(null=True, blank=True)
+    conclusion_date = models.DateField(null=True, blank=True)
+    done = models.BooleanField(null=False, blank=False, default=False)
+    fk_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reward = models.DecimalField(max_digits=10, decimal_places=0)
+    def __str__(self):
+        return f"Goal [description = {self.description}]"
+    
+
+
+
+#class Course(models.Model):
+#    name = models.CharField(max_length=300, null=False, blank=False)
+#    conclusion_date = models.DateField(null=True, blank=True)
+#    def __str__(self):
+#        return f"Couse [name = {self.name}]"
+
+
+#class Book(models.Model):
+#    name = models.CharField(max_length=300, null=False, blank=False)
+#    conclusion_date = models.DateField(null=True, blank=True)
+#    conclusion_date = models.DateField(default = datetime.now, null=True, blank=True)
+#    def __str__(self):
+#        return f"Book [name = {self.name}]"
