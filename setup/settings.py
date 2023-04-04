0472077885ecaf5c794a8c83b768a354316987a0
Sony Maia <sonymaia@MacBook-Pro-de-Sony.local>
2023-04-03 21:59:27 -0300

    icon

--- setup/settings.py
+++ setup/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "setup/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DISABLE_COLLECTSTATIC = 0
@@ -146,3 +146,7 @@ DISABLE_COLLECTSTATIC = 0
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
